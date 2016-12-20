853abb216ccdada8bd6d373bbc1a21a8b1c00212
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-12-19 22:53:29 -0600

    working 12 days

--- src/2016-12-19-easy/index.ts
+++ src/2016-12-19-easy/index.ts
@@ -1,2 +1,28 @@
-const Main = () => console.log('foo');
+const gifts = [
+    'Partridge in a Pear Tree',
+    'Turtle Doves',
+    'French Hens',
+    'Calling Birds',
+    'Golden Rings',
+    'Geese a Laying',
+    'Swans a Swimming',
+    'Maids a Milking',
+    'Ladies Dancing',
+    'Lords a Leaping',
+    'Pipers Piping',
+    'Drummers Drumming'
+];
+
+const wordify = (t: number, i: number) => (t - i === 1) ? (t === 1) ? 'a' : 'and a' : t - i;
+const logger = (t: number) => (g: string, i: number) => console.log(`${wordify(t, i)} ${g}`);
+
+const Main = () => {
+    for (let i = 1; i <= gifts.length; i++) {
+        console.log(`On the ${i} day of Christmas`);
+        console.log(`My true love gave to me...`);
+        [...gifts].reverse().slice(gifts.length - i).forEach(logger(i));
+        console.log('');
+    }
+}
+
 export default Main;
